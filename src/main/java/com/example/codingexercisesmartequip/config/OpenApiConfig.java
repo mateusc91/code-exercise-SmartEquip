f0f5026137853c52a7ff7c977ebeafddf1904f89
Mateus Cesar <mateus.cesar@agnes.com.br>
2024-09-17 11:41:07 -0300

    adding spring-docs (swagger)

---
+++ src/main/java/com/example/codingexercisesmartequip/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.example.codingexercisesmartequip.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sum Service API")
+                        .description("API to validate sum answers from random questions.")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project GitHub")
+                        .url("https://github.com/your-repo-link"));
+    }
+}
